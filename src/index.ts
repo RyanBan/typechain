0131e71e7802d9ea7b4fddd9c249ed438a8712b4
RyanBan <45275840+RyanBan@users.noreply.github.com>
2020-10-20 01:58:30 -0400

    Type, interface, classes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
-const name = "Ban",
-age = 44,
-gender="male";
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender:string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+
+const ban = new Human("Ban", 21, "male");
 
 //gender parameter is optional
-const sayHi = (name:string, age:number, gender:string): string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+const sayHi = (person: Human): string => {
+    return(`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`)
 };
-//expected 3 arguments, never happend in js
-sayHi(name, age, gender);
+console.log(sayHi(ban));
 
 
-export{};
+export { };
